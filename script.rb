c4da903c3f235f1c8e0a102d4b64a04c20ebc952
Simon Lacroix <simon@miaou.be>
2020-04-29 21:47:36 +0200

    Reset git history

--- script.rb
+++ script.rb
@@ -1,5 +1,169 @@
-File.open('act', 'w') { |file| file.write(['right', 'left', 'up', 'down'].sample) }
+# frozen_string_literal: true
 
-system('git add -A')
-system('git commit -m "some message"')
-system('git push orign master')
+# CONFIG
+# ======
+
+PLAYER_EMOJI = '😺'
+PLAYER_NAME = 'SimonMiaou'
+
+# CODE
+#=====
+
+require 'csv'
+require 'digest'
+require 'json'
+require 'net/http'
+require 'uri'
+
+system('rubocop -a')
+
+CELL_TO_EMOJI = {
+  'cb' => '🔵',
+  'cg' => '🟢',
+  'cr' => '🔴',
+  'ub' => '🟦',
+  'ug' => '🟩',
+  'ur' => '🟥',
+  'ux' => '⬛️'
+}.freeze
+TEAM_TO_STRING = {
+  'cb' => '🔵 Blue',
+  'cg' => '🟢 Green',
+  'cr' => '🔴 Red'
+}.freeze
+
+def find_best_move
+  frontier = [@current_position]
+  came_from = {}
+  targets = %w[ub ug ur ux]
+  targets.delete(targets.find { |t| t[1] == @team[1] })
+
+  position = nil
+
+  until frontier.empty?
+    position = frontier.shift
+
+    break if targets.include?(@map[position[:y]][position[:x]])
+
+    get_neighbors(position).each do |next_position|
+      next if @map[next_position[:y]][next_position[:x]][0] == 'c'
+
+      unless came_from.keys.include?(next_position)
+        frontier << next_position
+        came_from[next_position] = position
+      end
+    end
+  end
+
+  return 'idle' if position.nil?
+
+  position = came_from[position] while came_from[position] != @current_position
+
+  if position[:x] == @current_position[:x] && position[:y] > @current_position[:y]
+    'down'
+  elsif position[:x] == @current_position[:x] && position[:y] < @current_position[:y]
+    'up'
+  elsif position[:x] > @current_position[:x] && position[:y] == @current_position[:y]
+    'right'
+  elsif position[:x] < @current_position[:x] && position[:y] == @current_position[:y]
+    'left'
+  else
+    'idle'
+  end
+end
+
+def get_neighbors(position)
+  min_x = 0
+  min_y = 0
+  max_x = @map.first.size - 1
+  max_y = @map.size - 1
+
+  neighbors = []
+
+  neighbors << { x: position[:x] + 1, y: position[:y] }
+  neighbors << { x: position[:x] - 1, y: position[:y] }
+  neighbors << { x: position[:x], y: position[:y] + 1 }
+  neighbors << { x: position[:x], y: position[:y] - 1 }
+
+  neighbors.reject! do |n|
+    n[:x] < min_x || n[:x] > max_x ||
+      n[:y] < min_y || n[:y] > max_y
+  end
+
+  neighbors.sort_by { |n| Digest::MD5.hexdigest(n.to_json) }
+end
+
+def move(direction)
+  File.open('act', 'w') { |file| file.write(direction) }
+end
+
+def print_map
+  @map.each_with_index do |row, y|
+    row.each_with_index do |cell, x|
+      if x == @current_position[:x] && y == @current_position[:y]
+        print PLAYER_EMOJI
+        next
+      end
+      print CELL_TO_EMOJI[cell]
+    end
+    print "\n"
+  end
+end
+
+def print_stats
+  counters = {
+    'cb' => 0,
+    'ub' => 0,
+    'cg' => 0,
+    'ug' => 0,
+    'cr' => 0,
+    'ur' => 0,
+    'ux' => 0
+  }
+
+  @map.each do |row|
+    row.each do |cell|
+      counters[cell] ||= 0
+      counters[cell] += 1
+    end
+  end
+
+  counters.each do |k, v|
+    next if v.zero?
+
+    print "| #{CELL_TO_EMOJI[k]} #{v} "
+  end
+  print "|\n"
+end
+
+# MAIN LOOP
+# =========
+
+loop do
+  system('cd gitland && git pull')
+
+  @map = CSV.parse(File.read('gitland/map'))
+  @team = File.read("gitland/players/#{PLAYER_NAME}/team")
+  @current_position = { x: File.read("gitland/players/#{PLAYER_NAME}/x").to_i,
+                        y: File.read("gitland/players/#{PLAYER_NAME}/y").to_i }
+
+  next_move = find_best_move
+
+  puts '===================='
+  puts "| Player: #{PLAYER_NAME}"
+  puts "| Team: #{TEAM_TO_STRING[@team]}"
+  puts "| Position: #{@current_position[:x]}, #{@current_position[:y]}"
+  puts "| Next move: #{next_move}"
+
+  print_map
+
+  print_stats
+
+  move(next_move)
+
+  system('git add -A')
+  system("git commit -m \"Update #{Time.now}\"")
+  system('git push')
+
+  sleep 30
+end
---
+++ start.sh
@@ -0,0 +1,2 @@
+git pull
+ruby script.rb
