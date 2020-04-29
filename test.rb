49748724d3bb7867b60d1ca576ec1a6059d50986
Simon Lacroix <simon@miaou.be>
2020-04-30 01:21:06 +0200

    Update 2020-04-30 01:21:06 +0200

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'digest'
 require 'json'
@@ -7,56 +9,37 @@ require 'uri'
 PLAYER_EMOJI = '😺'
 PLAYER_NAME = 'SimonMiaou'
 CELL_TO_EMOJI = {
-'cb' => '🔵',
-'cg' => '🟢',
-'cr' => '🔴',
-'ub' => '🟦',
-'ug' => '🟩',
-'ur' => '🟥',
-'ux' => '⬛️'
+  'cb' => '🔵',
+  'cg' => '🟢',
+  'cr' => '🔴',
+  'ub' => '🟦',
+  'ug' => '🟩',
+  'ur' => '🟥',
+  'ux' => '⬛️'
 }.freeze
 TEAM_TO_STRING = {
-'cb' => '🔵 Blue',
-'cg' => '🟢 Green',
-'cr' => '🔴 Red'
+  'cb' => '🔵 Blue',
+  'cg' => '🟢 Green',
+  'cr' => '🔴 Red'
 }.freeze
 
-
-def print_map
-@map.each_with_index do |row, y|
-row.each_with_index do |cell, x|
-if x == @current_position[:x] && y == @current_position[:y]
-print PLAYER_EMOJI
-next
-end
-print CELL_TO_EMOJI[cell]
-end
-print "\n"
-end
-end
-
-
-
-
-
-
-
-
-
+system('rubocop -a')
 
 @map = CSV.parse(File.read('gitland/map'))
 @team = File.read("gitland/players/#{PLAYER_NAME}/team")
 @current_position = { x: File.read("gitland/players/#{PLAYER_NAME}/x").to_i,
-y: File.read("gitland/players/#{PLAYER_NAME}/y").to_i }
+                      y: File.read("gitland/players/#{PLAYER_NAME}/y").to_i }
 
-
-
-
-print_map
-
-
-
-
+@map.each_with_index do |row, y|
+  row.each_with_index do |cell, x|
+    if x == @current_position[:x] && y == @current_position[:y]
+      print PLAYER_EMOJI
+      next
+    end
+    print CELL_TO_EMOJI[cell]
+  end
+  print "\n"
+end
 
 targets = %w[ub ug ur]
 targets.delete(targets.find { |t| t[1] == @team[1] })
@@ -70,18 +53,17 @@ uncontrolled.delete(uncontrolled.find { |t| t[1] == @team[1] })
 position = nil
 
 until frontier.empty?
-position = frontier.shift
-puts "/ #{position}"
+  position = frontier.shift
+  puts "/ #{position}"
 
+  get_neighbors(position).each do |next_position|
+    next unless uncontrolled.include?(@map[next_position[:y]][next_position[:x]])
 
-get_neighbors(position).each do |next_position|
-next unless uncontrolled.include?(@map[next_position[:y]][next_position[:x]])
-
-unless came_from.keys.include?(next_position)
-frontier << next_position
-came_from[next_position] = position
-end
-end
+    unless came_from.keys.include?(next_position)
+      frontier << next_position
+      came_from[next_position] = position
+    end
+  end
 end
 
 return 'idle' if position.nil?
@@ -89,5 +71,3 @@ return 'idle' if position.nil?
 puts "// #{position}"
 
 puts came_from
-
-
