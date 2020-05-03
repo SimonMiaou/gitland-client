e07fa575101ca76be3502edbdfd7a08e71a00923
Simon Lacroix <simon@miaou.be>
2020-05-03 21:02:38 +0200

    Update 2020-05-03 21:02:38 +0200

--- script.rb
+++ script.rb
@@ -130,10 +130,10 @@ def get_neighbors(position)
   max_y = @map.size - 1
 
   # Avoid borders
-  min_x = [min_x + 1, @current_position[:x]].min
-  min_y = [min_y + 1, @current_position[:y]].min
-  max_x = [max_x - 1, @current_position[:x]].max
-  max_y = [max_y - 1, @current_position[:y]].max
+  # min_x = [min_x + 1, @current_position[:x]].min
+  # min_y = [min_y + 1, @current_position[:y]].min
+  # max_x = [max_x - 1, @current_position[:x]].max
+  # max_y = [max_y - 1, @current_position[:y]].max
 
   neighbors = []
 
